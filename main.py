b82c050f2b1a6d97150bc746fccb448767dd1375
Artem Netepin <artnetepin@gmail.com>
2026-06-28 18:42:20 +0300

    Draw player and add moving logic

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -15,7 +22,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
