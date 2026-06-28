13539f2cf0d5889f4f7caad0f0c4f98351d37574
Artem Netepin <artnetepin@gmail.com>
2026-06-28 19:40:55 +0300

    Add asteroid splitting logic

--- main.py
+++ main.py
@@ -48,6 +48,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
         for sprite in drawable:
             sprite.draw(screen)
 
